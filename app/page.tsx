3db000d17c2a7dee075842ce18333f14b0b2ade3
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-09-12 06:23:13 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/Link'
+import Link from 'next/link'
 import VideoPlayer from '@/components/VideoPlayer';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
