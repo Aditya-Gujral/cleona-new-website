7a5b4a6d99f93b38c087ffacf852ecdcbef579d0
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-09-28 04:14:40 -0400

    Delete .next directory

--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
->>>>>>> origin/main
