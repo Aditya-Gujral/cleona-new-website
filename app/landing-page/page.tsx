3e8bfa28eb4a07c1c814368d2406efaaa4cd7500
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-09-28 04:44:28 -0400

    Update page.tsx

--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -8,7 +8,6 @@ import VideoPlayer from '@/components/VideoPlayer';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
-
 export default function Page() {
   const videoUrl = "https://cleona-video.s3.amazonaws.com/CLEONA+Vid.mp4";
 
@@ -27,20 +26,10 @@ export default function Page() {
     textAlign: 'left' as const,
   };
 
-
-  const subHeadingStyle = {
-    fontSize: '3.5em',
-    fontWeight: 'bold',
-    margin: '0 1em',
-    textAlign: 'left' as const,
-  };
-
   return (
     <div style={{ textAlign: 'center' as const, fontFamily: 'Arial, sans-serif' }}>
-      <div><Header /></div>
-      <div>
-        <VideoPlayer videoSrc={videoUrl} />
-      </div>
+      <Header />
+      <VideoPlayer videoSrc={videoUrl} />
 
       <h2 style={{ fontSize: '5em', fontWeight: 'bold', color: '#007073', margin: '2em 0 0 0' }}>
         The AI platform for <br /> care operations
@@ -70,53 +59,48 @@ export default function Page() {
       <p style={{ fontSize: '2em', margin: '1em 1em 0 0', color: '#a6a6a6' }}>
         Powers personalized patient engagement across channels, including <br /> web app, patient portal and your website.
       </p>
-      <ul style={{ display: 'flex', justifyContent: 'space-around', listStyleType: 'none', padding: '10em 0' }}>
-        <li style={{ fontSize: '1em', margin: '0.5em', textAlign: 'center' as const }}>
-          <Image
-            src="/img/tickmark.png"
-            alt="Tick mark"
-            width={50}
-            height={50}
-            style={{ display: 'block', margin: '0 auto', height: '5em', width: '5em' }}
-          />
-          <h4 style={{ fontSize: '3em', fontWeight: 'bold', margin: '2em 0' }}>
-            Engage and keep <br /> more <br /> patients
-          </h4>
-          <p style={{ fontSize: '2em', margin: '1em 0' }}>
-            Remove access barriers by <br />​providing a 24/7 self-serve <br />​experience
-          </p>
-        </li>
-        <li style={{ fontSize: '1em', margin: '0.5em', textAlign: 'center' as const }}>
-          <Image
-            src="/img/tickmark.png"
-            alt="Tick mark"
-            width={50}
-            height={50}
-            style={{ display: 'block', margin: '0 auto', height: '5em', width: '5em' }}
-          />
-          <h4 style={{ fontSize: '3em', fontWeight: 'bold', margin: '2em 0' }}>
-            Reduce <br />resource <br /> costs
-          </h4>
-          <p style={{ fontSize: '2em', margin: '1em 0' }}>
-            Deflect calls from the <br /> contact center.
-          </p>
-        </li>
-        <li style={{ fontSize: '1em', margin: '0.5em', textAlign: 'center' as const }}>
-          <Image
-            src="/img/tickmark.png"
-            alt="Tick mark"
-            width={50}
-            height={50}
-            style={{ display: 'block', margin: '0 auto', height: '5em', width: '5em' }}
-          />
-          <h4 style={{ fontSize: '3em', fontWeight: 'bold', margin: '2em 0' }}>
-            Automate <br /> manual <br />work
-          </h4>
-          <p style={{ fontSize: '2em', margin: '1em 0' }}>
-            Eliminate endless back-and-<br />forth phone calls for patients <br /> and staff.
-          </p>
-        </li>
-      </ul>
+      <ul style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '10em 0',
+          listStyleType: 'none'
+        }}>
+          {['Engage and keep more patients', 'Reduce resource costs', 'Automate manual work'].map((item, index) => (
+            <li key={index} style={{
+              margin: '1em 0',
+              textAlign: 'center',
+              fontSize: '1.5em',
+            }}>
+              <Image
+                src="/img/tickmark.png"
+                alt="Tick mark"
+                width={50}
+                height={50}
+                style={{
+                  display: 'block',
+                  margin: '0 auto',
+                  height: '5em',
+                  width: '5em'
+                }}
+              />
+              <h4 style={{
+                fontSize: '1.5em',
+                fontWeight: 'bold'
+              }}>
+                {item}
+              </h4>
+              <p style={{
+                fontSize: '1.2em',
+                margin: '0'
+              }}>
+                {index === 0 ? 'Remove access barriers by providing a 24/7 self-serve experience' :
+                  index === 1 ? 'Deflect calls from the contact center.' :
+                  'Eliminate endless back-and-forth phone calls for patients and staff.'}
+              </p>
+            </li>
+          ))}
+        </ul>
 
       <h2 style={headingStyle}>
         Patient navigation
@@ -212,88 +196,23 @@ export default function Page() {
         style={{ display: 'block', margin: '1em auto', width: '80%' }}
       />
 
-      <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin: '5em 0 0 1em', color: '#007073' , textAlign: 'left' as const}}>
+      <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin: '5em 0 0 1em', color: '#007073', textAlign: 'left' as const }}>
         Setup
       </h2>
-      <h3 style={{ fontSize: '3em', fontWeight: 'bold', margin: '0 1em', textAlign: 'left' as const }}>
-        Configure in minutes and fully customize for your brand
-     
-
+      <h3 style={{ fontSize: '1.5em', margin: '0 0 0 1em', color: '#007073', textAlign: 'left' as const }}>
+        Get up and running quickly with an intuitive setup process.
       </h3>
-      <p style={{ fontSize: '2.8em', margin: '0 1.5em', color: '#a6a6a6', textAlign: 'left' as const}}>
-        Customize the look and feel of the Assistant and train it on your organization’s resources through a no-code interface.
+      <p style={{ fontSize: '2em', margin: '0 1em', color: '#a6a6a6', textAlign: 'left' as const }}>
+        Give us access to your EHR or website, and we’ll work with you to define your unique patient journeys.
       </p>
-      <Image
-        src="/img/Set up.png"
-        alt="Set up screen"
-        width={500}
-        height={500}
-        style={{ display: 'block', margin: '1em auto', width: '80%' }}
-      />
-
-      <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin: '5em  0 0 1em', color: '#007073' , textAlign: 'left' as const}}>
-        Integrations
+      <h2 style={{ fontSize: '2.8em', fontWeight: 'bold', margin: '4em 0 0 1em', color: '#007073', textAlign: 'left' as const }}>
+        Data security
       </h2>
-      <h3 style={{ fontSize: '3em', fontWeight: 'bold', margin: '0 1em', textAlign: 'left' }}>
-        Hyper-personalize patient experiences by connecting your EHR
-      </h3>
-      <p style={{ fontSize: '2.8em', margin: '0 1.5em', color: '#a6a6a6', textAlign: 'left' }}>
-        Empower patients to schedule appointments, pay bills, receive real-time information, and more without staff intervention.
+      <p style={{ fontSize: '2em', margin: '0 1em', color: '#a6a6a6', textAlign: 'left' as const }}>
+        We are HIPAA compliant and ensure that your patient data is safe.
       </p>
 
-      <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin:'5em  0 0 1em', color: '#007073' , textAlign: 'left' as const }}>
-        Deployment
-      </h2>
-      <h3 style={{ fontSize: '3em', fontWeight: 'bold', margin: '0 1em', textAlign: 'left' as const }}>
-        Embed into your existing website, mobile app, and patient portal
-      </h3>
-      <p style={{ fontSize: '2.5em', margin: '0 1.5em', color: '#a6a6a6', textAlign: 'left' as const }}>
-        Enhance your existing channels with conversational AI.
-      </p>
-      <Image
-        src="/img/Reproductive Assistant.gif"
-        alt="Deployment screen"
-        width={500}
-        height={500}
-        style={{ display: 'block', margin: 'auto', width: '80%' }}
-      />
-
-      <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin: '5em  0 0 1em', color: '#007073' , textAlign: 'left' as const }}>
-        Analytics
-      </h2>
-      <h3 style={{ fontSize: '3em', fontWeight: 'bold', margin: '0 1em', textAlign: 'left' as const}}>
-        Uncover exactly what your patients are looking for
-      </h3>
-      <p style={{ fontSize: '2.5em', margin: '0 1.5em', color: '#a6a6a6' , textAlign: 'left' as const}}>
-        Build a deeper understanding of the most common questions and use cases for your website, mobile app, and portal.
-      </p>
-      <Image
-        src="/img/Analytics.png"
-        alt="Analytics screen"
-        width={500}
-        height={500}
-        style={{ display: 'block', margin: '1em auto', width: '80%' }}
-      />
-      {/*<Link href="/insights"> <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin: '0', color: '#007073' }}>
-        Insights
-      </h2>
-      <h3 style={{ fontSize: '3em', fontWeight: 'bold', margin: '0.5em 0' }}>
-        Learn how AI is being leveraged to reshape care
-      </h3></Link>
-      <h2 style={{ fontSize: '2em', fontWeight: 'bold', margin: '0', color: '#007073' }}>
-        Insights
-      </h2>
-      <h3 style={{ fontSize: '3em', fontWeight: 'bold', margin: '0.5em 0' }}>
-        Learn how AI is being leveraged to reshape care
-      </h3>*/}
-
-      <h2 style={{ fontSize: '5em', fontWeight: 'bold', color: '#007073' }}>How we partner</h2>
-      <p style={{ fontSize: '2em', margin: '1em 0' , color: '#007073'}}>CLEONA drives demonstrable ROI for partners through our proprietary solutions for realizing financial outcomes from AI investments.</p>
-
-      <div>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 }
-
