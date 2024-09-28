04cf2fc13c02b09cc5b2b610e0c81041121b5ee1
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-09-28 02:19:49 -0400

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,38 +1,35 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "./lib/sanity/schemas";
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './lib/sanity/schemas';
 import {
   projectId,
   dataset,
   previewSecretId
-} from "./lib/sanity/config";
-import settings from "./lib/sanity/schemas/settings";
+} from './lib/sanity/config';
+import settings from './lib/sanity/schemas/settings';
 import {
   pageStructure,
   singletonPlugin
-} from "./lib/sanity/plugins/settings";
-import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
-import { table } from "@sanity/table";
-import { codeInput } from "@sanity/code-input";
+} from './lib/sanity/plugins/settings';
+import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash';
+import { table } from '@sanity/table';
+import { codeInput } from '@sanity/code-input';
 
-export const PREVIEWABLE_DOCUMENT_TYPES: string[] = ["post"];
-console.log(projectId);
+export const PREVIEWABLE_DOCUMENT_TYPES: string[] = ['post'];
 
 export default defineConfig({
-  name: "default",
-  title: "Stablo Template",
-  basePath: "/studio",
+  name: 'default',
+  title: 'Stablo Template',
+  basePath: '/insights/studio',
   projectId: projectId,
   dataset: dataset,
 
   plugins: [
     deskTool({
-      structure: pageStructure([settings])
-      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
-      // defaultDocumentNode: previewDocumentNode({ apiVersion, previewSecretId }),
+      structure: pageStructure as any, // Ensure correct type casting if necessary
     }),
-    singletonPlugin(["settings"]),
+    singletonPlugin(['settings']),
     visionTool(),
     unsplashImageAsset(),
     table(),
@@ -43,3 +40,4 @@ export default defineConfig({
     types: schemaTypes
   }
 });
+
