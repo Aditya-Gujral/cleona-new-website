a3d3fdaaf94c84fd0f3f36c3bc4a187fd054ac48
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-09-28 04:25:01 -0400

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,25 +2,13 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './lib/sanity/schemas';
-import {
-  projectId,
-  dataset,
-  previewSecretId
-} from './lib/sanity/config';
-import settings from './lib/sanity/schemas/settings';
-import {
-  pageStructure,
-  singletonPlugin
-} from './lib/sanity/plugins/settings';
+import { projectId, dataset, previewSecretId } from './lib/sanity/config';
+import { pageStructure, singletonPlugin } from './lib/sanity/plugins/settings';
 import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash';
 import { table } from '@sanity/table';
 import { codeInput } from '@sanity/code-input';
 
 export const PREVIEWABLE_DOCUMENT_TYPES: string[] = ['post'];
-<<<<<<< HEAD
-=======
-console.log(projectId);
->>>>>>> origin/main
 
 export default defineConfig({
   name: 'default',
@@ -31,12 +19,7 @@ export default defineConfig({
 
   plugins: [
     deskTool({
-<<<<<<< HEAD
       structure: pageStructure as any, // Ensure correct type casting if necessary
-=======
-      structure: pageStructure as any, // Adjusted type casting
-      // defaultDocumentNode: previewDocumentNode({ apiVersion, previewSecretId }),
->>>>>>> origin/main
     }),
     singletonPlugin(['settings']),
     visionTool(),
@@ -49,3 +32,4 @@ export default defineConfig({
     types: schemaTypes
   }
 });
+
